162a5c81591ab9344292e2e773975f9174a81fe8
iqbalmln <miqblmaulana@gmail.com>
2021-10-25 12:29:39 +0700

    bouncing icon on landing page

--- src/sections/Landing.vue
+++ src/sections/Landing.vue
@@ -10,7 +10,7 @@
       <h2 class="text-2xl md:text-5xl">‘Asa Ananta’</h2>
     </div>
 
-    <div>
+    <div class="bounce">
       <i class='bx bx-chevrons-down text-4xl'></i>
     </div>
   </div>
@@ -22,3 +22,38 @@ export default {
   name: 'LandingSection'
 }
 </script>
+<style>
+.bounce {
+	-webkit-border-radius:50%;
+	-moz-border-radius:50%;
+	-ms-border-radius:50%;
+	border-radius:50%;
+	animation: bounce 1s infinite;
+	-webkit-animation: bounce 1s infinite;
+	-moz-animation: bounce 1s infinite;
+	-o-animation: bounce 1s infinite;
+}
+ 
+@-webkit-keyframes bounce {
+	0%, 20%, 50%, 80%, 100% {-webkit-transform: translateY(0);}	
+	40% {-webkit-transform: translateY(-30px);}
+	60% {-webkit-transform: translateY(-15px);}
+}
+ 
+@-moz-keyframes bounce {
+	0%, 20%, 50%, 80%, 100% {-moz-transform: translateY(0);}
+	40% {-moz-transform: translateY(-30px);}
+	60% {-moz-transform: translateY(-15px);}
+}
+ 
+@-o-keyframes bounce {
+	0%, 20%, 50%, 80%, 100% {-o-transform: translateY(0);}
+	40% {-o-transform: translateY(-30px);}
+	60% {-o-transform: translateY(-15px);}
+}
+@keyframes bounce {
+	0%, 20%, 50%, 80%, 100% {transform: translateY(0);}
+	40% {transform: translateY(-30px);}
+	60% {transform: translateY(-15px);}
+}
+</style>
